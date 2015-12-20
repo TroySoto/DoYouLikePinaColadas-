699c79e1529884bc274f7fc5c8e8060e7c2e0817
TSoto1 <student@TSoto1.local>
2015-12-20 12:49:48 -0600

    Initial Commit

---
+++ Do You Like Pina Coladas? 3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Do You Like Pina Coladas? 3
+//
+//  Created by TSoto1 on 12/20/15.
+//  Copyright (c) 2015 TSoto1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
